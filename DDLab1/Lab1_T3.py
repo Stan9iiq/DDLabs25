6c99a871963d9189cfe06e51a6d4b34766422fed
Andrey <144437130+Stan9iiq@users.noreply.github.com>
2025-05-10 20:19:31 +0300

    Add files via upload

---
+++ DDLab1/Lab1_T3.py
@@ -0,0 +1,9 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+centre = len(list_players) // 2
+
+fteam = list_players[:centre]
+steam = list_players[centre:]
+
+print(fteam)
+print(steam)
