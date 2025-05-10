cd28f231df55ee2d11ce9cf5d013c2aeb94401f1
Andrey <144437130+Stan9iiq@users.noreply.github.com>
2025-05-10 20:32:55 +0300

    Delete DDLab1 directory

--- DDLab1/Lab1_T3.py
+++
@@ -1,9 +0,0 @@
-list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-
-centre = len(list_players) // 2
-
-fteam = list_players[:centre]
-steam = list_players[centre:]
-
-print(fteam)
-print(steam)
